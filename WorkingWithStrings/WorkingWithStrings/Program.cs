9e1f6c6d478eecb6f65dfe52824f55e6acaffe39
Daniel Plawgo <daniel@plawgo.pl>
2018-07-09 14:44:01 +0200

    dodanie przykładu

---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationName = ConfigurationManager.AppSettings[SettingsNames.ApplicationName];
+
+            var message = string.Format(ApplicationResources.HelloMessage, applicationName);
+
+            Console.WriteLine(message);
+        }
+    }
+}
